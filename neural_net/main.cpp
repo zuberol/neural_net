26c6256a6c0fd65147b799f55573db8a6922ce17
Jakub <zuber.kuba@gmail.com>
2019-05-30 20:38:15 +0200

    Initial Commit

---
+++ neural_net/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  neural_net
+//
+//  Created by Jakub Żuber on 30/05/2019.
+//  Copyright © 2019 Jakub Żuber. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
